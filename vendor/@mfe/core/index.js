9979096bed63ccd9cdacb790d938b06b35e15ad9
Jaye Marshall <github@jaye.ch>
2026-08-21 11:50:32 +0000

    consume mfe-framework as a git submodule (build dist into vendor/@mfe at deploy)
    - Add mfe-framework as a git submodule.
    - Build script now builds the submodule (npm run build in mfe-framework) and
      copies its dist into vendor/@mfe/ (a build artifact, now gitignored).
    - Import map still points at /vendor/@mfe/ (served statically).
    - Removed the previously-committed vendor/@mfe/ from git.
    - Verified: build compiles, real-browser 0 errors, MFE mounts (registry=1).

--- vendor/@mfe/core/index.js
+++
@@ -1,6 +0,0 @@
-export { makeRef } from './ref.js';
-export { SLOT_SELECTOR, querySlotElements, getSlotName, getSlotProps, collect, } from './dom.js';
-export { loadTemplate } from './template.js';
-export { createRegistry } from './registry.js';
-export { reconcile } from './reconcile.js';
-//# sourceMappingURL=index.js.map
